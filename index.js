32bada52384daa762b31519be91bccd3f2d0504d
Andrey Popp <8mayday@gmail.com>
2013-07-18 01:22:28 +0400

    export plain component

--- index.js
+++ index.js
@@ -3,28 +3,27 @@
  */
 
 var datetime = require('datetime');
+var React = require('react-tools/build/modules/react');
 
-module.exports = function(React) {
-  return React.createClass({
-    render: function() {
-      var value = this.props.value,
-          machineReadable,
-          humanReadable,
-          formatter;
+module.exports = React.createClass({
+  render: function() {
+    var value = this.props.value,
+        machineReadable,
+        humanReadable,
+        formatter;
 
-      if (Object.prototype.toString.call(value) == '[object String]') {
-        value = new Date(value);
-      }
-
-      machineReadable = datetime.format(value, "%Y-%m-%dT%H:%M:%S");
-
-      if (this.props.relative || this.props.format) {
-        formatter = this.props.relative ? datetime.formatAgo : datetime.format;
-        humanReadable = formatter(value, this.props.format);
-        return React.DOM.time( {datetime:machineReadable}, humanReadable);
-      } else {
-        return React.DOM.time(null, machineReadable);
-      }
+    if (Object.prototype.toString.call(value) == '[object String]') {
+      value = new Date(value);
     }
-  });
-};
+
+    machineReadable = datetime.format(value, "%Y-%m-%dT%H:%M:%S");
+
+    if (this.props.relative || this.props.format) {
+      formatter = this.props.relative ? datetime.formatAgo : datetime.format;
+      humanReadable = formatter(value, this.props.format);
+      return React.DOM.time( {datetime:machineReadable}, humanReadable);
+    } else {
+      return React.DOM.time(null, machineReadable);
+    }
+  }
+});
--- index.jsx
+++ index.jsx
@@ -3,28 +3,27 @@
  */
 
 var datetime = require('datetime');
+var React = require('react-tools/build/modules/react');
 
-module.exports = function(React) {
-  return React.createClass({
-    render: function() {
-      var value = this.props.value,
-          machineReadable,
-          humanReadable,
-          formatter;
+module.exports = React.createClass({
+  render: function() {
+    var value = this.props.value,
+        machineReadable,
+        humanReadable,
+        formatter;
 
-      if (Object.prototype.toString.call(value) == '[object String]') {
-        value = new Date(value);
-      }
-
-      machineReadable = datetime.format(value, "%Y-%m-%dT%H:%M:%S");
-
-      if (this.props.relative || this.props.format) {
-        formatter = this.props.relative ? datetime.formatAgo : datetime.format;
-        humanReadable = formatter(value, this.props.format);
-        return <time datetime={machineReadable}>{humanReadable}</time>;
-      } else {
-        return <time>{machineReadable}</time>;
-      }
+    if (Object.prototype.toString.call(value) == '[object String]') {
+      value = new Date(value);
     }
-  });
-};
+
+    machineReadable = datetime.format(value, "%Y-%m-%dT%H:%M:%S");
+
+    if (this.props.relative || this.props.format) {
+      formatter = this.props.relative ? datetime.formatAgo : datetime.format;
+      humanReadable = formatter(value, this.props.format);
+      return <time datetime={machineReadable}>{humanReadable}</time>;
+    } else {
+      return <time>{machineReadable}</time>;
+    }
+  }
+});
