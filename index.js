5126ff4b3a728200f6ba91f613dfc3755df9317d
Andrey Popp <8mayday@gmail.com>
2014-02-16 17:10:22 +0400

    Fix datetime formatting, props transfering, ...

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
 var datetime = require('datetime');
 var React = require('react');
 
+function leadingZero(n, leading) {
+  return n >= 10 ? n : (leading ? leading : '0')+n;
+}
+
+function formatOffset(d) {
+  var offset = d.getTimezoneOffset();
+  var hour = leadingZero(Math.round(Math.abs(offset / 60)));
+  var min = leadingZero(offset % 60);
+  return (offset > 0 ? '-' : '+') + hour + ':' + min;
+}
+
 module.exports = React.createClass({
   render: function() {
-    var value = this.props.value,
-        machineReadable,
-        humanReadable,
-        formatter;
+    var value = this.props.value;
 
     if (Object.prototype.toString.call(value) == '[object String]') {
       value = new Date(value);
     }
 
-    machineReadable = datetime.format(value, "%Y-%m-%dT%H:%M:%S");
+    var machineReadable = datetime.format(value, "%Y-%m-%dT%H:%M:%S");
+    // we can use %z formatter instead of formatOffset when
+    // https://github.com/joehewitt/datetime/pull/8 is merged
+    machineReadable += formatOffset(value);
+
+    var props = {};
+    for (var k in this.props) {
+      if (this.props.hasOwnProperty(k) &&
+          k !== 'value' &&
+          k !== 'relative' &&
+          k !== 'format')
+        props[k] = this.props[k];
+    }
 
     if (this.props.relative || this.props.format) {
-      formatter = this.props.relative ? datetime.formatAgo : datetime.format;
-      humanReadable = formatter(value, this.props.format);
-      return this.transferPropsTo(React.DOM.time(
-          {dateTime: machineReadable},
-          humanReadable));
+      var formatter = this.props.relative ? datetime.formatAgo : datetime.format;
+      var humanReadable = formatter(value, this.props.format);
+      props.dateTime = machineReadable;
+      return React.DOM.time(props, humanReadable);
     } else {
-      return this.transferPropsTo(React.DOM.time(
-          null,
-          machineReadable));
+      return React.DOM.time(props, machineReadable);
     }
   }
 });
--- spec.js
+++ spec.js
@@ -3,23 +3,36 @@ var Timestamp = require('./index');
 var React = require('react');
 
 describe('react-time', function() {
-  var date = new Date('1987-05-08');
+  var date = new Date('1987-05-08T05:00:00+05:00');
 
   it('renders date', function() {
-    React.renderComponentToString(Timestamp({value: date}), function(markup) {
+    var c = Timestamp({value: date});
+    React.renderComponentToString(c, function(markup) {
       assert(/1987\-05\-08/.test(markup));
     });
   });
 
   it('renders date in specified format', function() {
-    React.renderComponentToString(Timestamp({value: date, format: "%Y%Y"}), function(markup) {
+    var c = Timestamp({value: date, format: "%Y%Y"});
+    React.renderComponentToString(c, function(markup) {
+      assert(/datetime="1987\-05\-08T05:00:00\+05:00"/.test(markup));
       assert(/19871987/.test(markup));
     });
   });
 
   it('renders date using relative format', function() {
-    React.renderComponentToString(Timestamp({value: date, relative: true}), function(markup) {
+    var c = Timestamp({value: date, relative: true});
+    React.renderComponentToString(c, function(markup) {
+      assert(/datetime="1987\-05\-08T05:00:00\+05:00"/.test(markup));
       assert(/May  8th, 1987/.test(markup));
+    });
+  });
+
+  it('transfers props down to DOM element', function() {
+    var c = Timestamp({value: date, relative: true, className: 'className'});
+    React.renderComponentToString(c, function(markup) {
+      assert(/May  8th, 1987/.test(markup));
+      assert(/class="className"/.test(markup));
     });
   });
 });
