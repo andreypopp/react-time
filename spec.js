6612a2ee8462d2c786de61f5a70ca95b211b67a1
Andrey Popp <8mayday@gmail.com>
2014-02-17 18:14:56 +0400

    Update code to new renderComponentToString API

--- spec.js
+++ spec.js
@@ -7,32 +7,28 @@ describe('react-time', function() {
 
   it('renders date', function() {
     var c = Timestamp({value: date});
-    React.renderComponentToString(c, function(markup) {
-      assert(/1987\-05\-08/.test(markup));
-    });
+    var markup = React.renderComponentToString(c);
+    assert(/1987\-05\-08/.test(markup));
   });
 
   it('renders date in specified format', function() {
     var c = Timestamp({value: date, format: "%Y%Y"});
-    React.renderComponentToString(c, function(markup) {
-      assert(/datetime="1987\-05\-08T05:00:00\+05:00"/.test(markup));
-      assert(/19871987/.test(markup));
-    });
+    var markup = React.renderComponentToString(c);
+    assert(/datetime="1987\-05\-08T05:00:00\+05:00"/.test(markup));
+    assert(/19871987/.test(markup));
   });
 
   it('renders date using relative format', function() {
     var c = Timestamp({value: date, relative: true});
-    React.renderComponentToString(c, function(markup) {
-      assert(/datetime="1987\-05\-08T05:00:00\+05:00"/.test(markup));
-      assert(/May  8th, 1987/.test(markup));
-    });
+    var markup = React.renderComponentToString(c);
+    assert(/datetime="1987\-05\-08T05:00:00\+05:00"/.test(markup));
+    assert(/May  8th, 1987/.test(markup));
   });
 
   it('transfers props down to DOM element', function() {
     var c = Timestamp({value: date, relative: true, className: 'className'});
-    React.renderComponentToString(c, function(markup) {
-      assert(/May  8th, 1987/.test(markup));
-      assert(/class="className"/.test(markup));
-    });
+    var markup = React.renderComponentToString(c);
+    assert(/May  8th, 1987/.test(markup));
+    assert(/class="className"/.test(markup));
   });
 });
