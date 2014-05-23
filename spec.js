87c4cae1627b474a0d700be4b35e4d333a331352
Adrian Lang <mail@adrianlang.de>
2014-05-23 22:54:02 +0200

    Make tests work independent of broken timezone support in JS' Date

--- spec.js
+++ spec.js
@@ -3,7 +3,7 @@ var Timestamp = require('./index');
 var React = require('react');
 
 describe('react-time', function() {
-  var date = new Date('1987-05-08T05:00:00+05:00');
+  var date = new Date(1987, 4, 8, 5, 0, 0, 0);
 
   it('renders date', function() {
     var c = Timestamp({value: date});
@@ -14,14 +14,14 @@ describe('react-time', function() {
   it('renders date in specified format', function() {
     var c = Timestamp({value: date, format: "%Y%Y"});
     var markup = React.renderComponentToString(c);
-    assert(/datetime="1987\-05\-08T05:00:00\+05:00"/.test(markup));
+    assert(/datetime="1987\-05\-08T05:00:00[+-]\d\d:\d\d"/.test(markup));
     assert(/19871987/.test(markup));
   });
 
   it('renders date using relative format', function() {
     var c = Timestamp({value: date, relative: true});
     var markup = React.renderComponentToString(c);
-    assert(/datetime="1987\-05\-08T05:00:00\+05:00"/.test(markup));
+    assert(/datetime="1987\-05\-08T05:00:00[+-]\d\d:\d\d"/.test(markup));
     assert(/May  8th, 1987/.test(markup));
   });
 
