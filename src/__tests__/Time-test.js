bf24bdaaceff71602e506f338933619b1f994ed1
Lars Nyström <nystrom.lars.egon@gmail.com>
2016-04-08 11:35:22 +0200

    Use ReactDOMServer.renderToString() in tests

--- src/__tests__/Time-test.js
+++ src/__tests__/Time-test.js
@@ -2,10 +2,11 @@
  * @copyright 2015, Andrey Popp <8mayday@gmail.com>
  */
 
-import assert   from 'power-assert';
-import Time     from '../Time';
-import moment   from 'moment';
-import React    from 'react';
+import assert         from 'power-assert';
+import Time           from '../Time';
+import moment         from 'moment';
+import React          from 'react';
+import ReactDOMServer from 'react-dom/server';
 
 // load "fr" locale for tests
 import 'moment/locale/fr';
@@ -17,71 +18,71 @@ describe('react-time', function() {
   let date = new Date(1987, 4, 8, 5, 0, 0, 0);
 
   it('renders date', function() {
-    let markup = React.renderToString(<Time value={date} />);
+    let markup = ReactDOMServer.renderToString(<Time value={date} />);
     assert(/1987\-05\-08/.test(markup));
   });
 
   it('renders date in specified format', function() {
-    let markup = React.renderToString(<Time value={date} format="YYYY YYYY" />);
+    let markup = ReactDOMServer.renderToString(<Time value={date} format="YYYY YYYY" />);
     assert(/datetime="1987\-05\-08T\d\d:\d\d:\d\d[+-]\d\d:\d\d"/.test(markup));
     assert(/1987 1987/.test(markup));
   });
 
   it('renders date using relative format', function() {
-    let markup = React.renderToString(<Time value={date} relative />);
+    let markup = ReactDOMServer.renderToString(<Time value={date} relative />);
     assert(/datetime="1987\-05\-08T\d\d:\d\d:\d\d[+-]\d\d:\d\d"/.test(markup));
     assert(/28 years ago/.test(markup), markup);
   });
 
   it('renders a title when using relative format', function() {
-    let markup = React.renderToString(<Time value={date} relative />);
+    let markup = ReactDOMServer.renderToString(<Time value={date} relative />);
     assert(/title="1987\-05\-08 05:00"/.test(markup));
   });
 
   it('renders the title in specified format', function() {
-    let markup = React.renderToString(<Time value={date} relative titleFormat="YYYY" />);
+    let markup = ReactDOMServer.renderToString(<Time value={date} relative titleFormat="YYYY" />);
     assert(/title="1987"/.test(markup));
   });
 
   it('does not render a title using absolute format', function() {
-    let markup = React.renderToString(<Time value={date} relative titleFormat="YYYY" />);
+    let markup = ReactDOMServer.renderToString(<Time value={date} relative titleFormat="YYYY" />);
     assert(/title/.test(markup));
   });
 
   it('accepts locale prop', function() {
-    let markup = React.renderToString(<Time value={date} relative locale="fr" />);
+    let markup = ReactDOMServer.renderToString(<Time value={date} relative locale="fr" />);
     assert(/datetime="1987\-05\-08T05:00:00[+-]\d\d:\d\d"/.test(markup));
     assert(/il y a 28 ans/.test(markup), markup);
   });
 
   it('transfers props down to DOM element', function() {
-    let markup = React.renderToString(<Time value={date} relative className="className" />);
+    let markup = ReactDOMServer.renderToString(<Time value={date} relative className="className" />);
     assert(/28 years ago/.test(markup), markup);
     assert(/class="className"/.test(markup));
   });
 
   it('allows passing milliseconds from epoch', function() {
-    let markup = React.renderToString(<Time value={1} />);
+    let markup = ReactDOMServer.renderToString(<Time value={1} />);
     assert(/1970\-01\-01/.test(markup));
   });
 
   it('allows passing string', function() {
-    let markup = React.renderToString(<Time value="1970-01-01" />);
+    let markup = ReactDOMServer.renderToString(<Time value="1970-01-01" />);
     assert(/1970\-01\-01/.test(markup));
   });
 
   it('allows passing string with custom format', function() {
-    let markup = React.renderToString(<Time value="1970/01/01" valueFormat="YYYY/mm/DD" />);
+    let markup = ReactDOMServer.renderToString(<Time value="1970/01/01" valueFormat="YYYY/mm/DD" />);
     assert(/1970\-01\-01/.test(markup));
   });
 
   it('allows passing moment instance', function() {
-    let markup = React.renderToString(<Time value={moment('1970-01-01')} />);
+    let markup = ReactDOMServer.renderToString(<Time value={moment('1970-01-01')} />);
     assert(/1970\-01\-01/.test(markup));
   });
 
   it('does not render current date if value is falsy', function() {
-    let markup = React.renderToString(<Time />);
+    let markup = ReactDOMServer.renderToString(<Time />);
     assert(/Invalid date/.test(markup));
   });
 });
